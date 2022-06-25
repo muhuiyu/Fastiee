9b7c7ab155681c70d0d4745b2c0bd24d291b9913
Grace, Mu-Hui Yu <muhuiyu.grace@gmail.com>
2022-06-25 08:40:12 +0800

    create xcdoe project

---
+++ Fastiee/Features/Home/Controllers/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  Fastiee
+//
+//  Created by Mu Yu on 6/25/22.
+//
+
+import UIKit
+
+class HomeViewController: ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViews()
+        configureConstraints()
+        configureGestures()
+        configureSignals()
+    }
+}
+// MARK: - View Config
+extension HomeViewController {
+    private func configureViews() {
+        
+    }
+    private func configureConstraints() {
+        
+    }
+    private func configureGestures() {
+        
+    }
+    private func configureSignals() {
+        
+    }
+}
+
